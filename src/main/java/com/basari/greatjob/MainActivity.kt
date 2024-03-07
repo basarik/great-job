fcd0b1785ca9f0c671a6c48c64145c0eda240aeb
tcbkubuzcu <Horasan123>
2024-03-07 14:39:26 +0300

    init commit

---
+++ src/main/java/com/basari/greatjob/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.basari.greatjob
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import com.basari.greatjob.ui.theme.GreatJobTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GreatJobTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+
+    Text(text = "asd", modifier = Modifier.run {
+        padding(
+        start = Dp(16F), end = Dp(16F), top = Dp(80F), bottom = Dp(8F)
+    )
+    })
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    GreatJobTheme {
+        Greeting("Android")
+    }
+}
